03939479666c7baa021e25f369434716f01f5ed3
chaddha1 <himanshuchaddha4u@gmail.com>
2022-06-12 21:57:48 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // This is the the time we do dirct assignment to this.state
+    this.state = {
+      lat: null,
+      errorMessage: "",
+    };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setState() to set the state
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error : {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude : {this.state.lat}</div>
+        }
+
+        return <div>Loading!</div>
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
